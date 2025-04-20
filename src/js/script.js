8d6e58e4cfcbdd109959f50d3065e5b1d01f10ac
rafael <rafinhaacarmona@gmail.com>
2025-04-20 20:50:38 -0300

    exercicio1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
+//questao 1 
+let nomeVinho;
+let tipoUva = "";
+let precoGarrafa = 59.90;
+
+console.log("=== Cadastro de Vinho ===");
+console.log("Nome do vinho:", nomeVinho);
+console.log("Tipo de uva:", tipoUva);
+console.log("Preço da garrafa: R$", precoGarrafa);
+
 
