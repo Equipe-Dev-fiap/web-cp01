94907d631584b18a59ce1ee44f8fd847a2e1c369
rafael <rafinhaacarmona@gmail.com>
2025-04-20 21:07:54 -0300

    exercicio 2

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,19 @@ console.log("Nome do vinho:", nomeVinho);
 console.log("Tipo de uva:", tipoUva);
 console.log("Preço da garrafa: R$", precoGarrafa);
 
+//questao 2 
+let tipoVinho = prompt("Qual o tipo de vinho? (Tinto ou Branco)");
+let idadeCliente = parseInt(prompt("Qual a sua idade?"));
+
+// Operações com operadores ==, !=, >=
+let ehTinto = tipoVinho == "Tinto";
+let naoEhBranco = tipoVinho != "Branco";
+let podeComprar = idadeCliente >= 18;
+
+console.log("Resultados:\n" +
+  "O vinho é tinto? " + ehTinto + "\n" +
+  "O vinho NÃO é branco? " + naoEhBranco + "\n" +
+  "O cliente pode comprar vinho? (idade >= 18): " + podeComprar);
+
+
 
